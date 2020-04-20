2563b74bfc8e8c1b74d046f6c6d4ff6868841879
Johee Chung <johee.chung@nike.com>
2020-04-19 23:07:46 -0700

    Visualize rating system with stars

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as faStarFull, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
 import yelp from '../serviceYelp';
 import './App.css';
 
@@ -20,6 +23,15 @@ function App() {
     return null;
   }
 
+  const countFullStars = Math.floor(sampleBusiness.rating);
+  const fullStars = [...Array(countFullStars)];
+
+  const countEmptyStars = 5 - Math.ceil(sampleBusiness.rating);
+  const emptyStars = [...Array(countEmptyStars)];
+
+  const countHalfStars = 5 - fullStars + emptyStars;
+  const halfStars = [...Array(countHalfStars)];
+
   return (
     <div className="card">
       <div className="header">
@@ -34,8 +46,12 @@ function App() {
         <div>{sampleBusiness.location.city + ', ' + sampleBusiness.location.zip_code}</div>
         <div>{sampleBusiness.display_phone}</div>
       </div>
-      <div>{sampleBusiness.rating}</div>
-      <div>{sampleBusiness.price}</div>
+      <div className="rating">
+        {fullStars.map((_, i) => <span key={'fullStar-' + i}><FontAwesomeIcon icon={faStarFull} /></span>)}
+        {halfStars.map((_, i) => <span key={'halfStar-' + i}><FontAwesomeIcon icon={faStarHalfAlt} /></span>)}
+        {emptyStars.map((_, i) => <span key={'emptyStar-' + i}><FontAwesomeIcon icon={faStarEmpty} /></span>)}
+      </div>
+      <div className="cost">{sampleBusiness.price}</div>
     </div>
   );
 }
