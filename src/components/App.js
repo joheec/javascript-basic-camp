6a0abb611e5ea27ccf113f71524632620f9ab12c
Johee Chung <johee.chung@nike.com>
2020-04-19 21:48:27 -0700

    Pull out values for the first business and create initial jsx

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,26 @@ function App() {
 
   // Prints businesses
   console.log({ businesses });
+  const sampleBusiness = businesses[0];
 
-  return null;
+  if (businesses.length === 0) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div>{sampleBusiness.name}</div>
+      <div>{sampleBusiness.categories[0].title}</div>
+      <img src={sampleBusiness.image_url} alt="Business" />
+      <div>{sampleBusiness.location.address1}</div>
+      <div>{sampleBusiness.location.address2}</div>
+      <div>{sampleBusiness.location.address3}</div>
+      <div>{sampleBusiness.location.city + ', ' + sampleBusiness.location.zip_code}</div>
+      <div>{sampleBusiness.display_phone}</div>
+      <div>{sampleBusiness.rating}</div>
+      <div>{sampleBusiness.price}</div>
+    </div>
+  );
 }
 
 export default App;
